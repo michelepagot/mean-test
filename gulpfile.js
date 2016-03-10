6e285c1ed0dda7fdae1a57d3244997b776176631
michelepagot <michele.pagot@hotmail.it>
2016-03-10 01:33:40 +0100

    More buildsystem refinement

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,16 @@
 var gulp = require('gulp');
+
+// include plug-ins
+var jshint = require('gulp-jshint');
 var mocha = require('gulp-mocha');
-	
+
+
+// JS hint task
+gulp.task('jshint', function() {
+  gulp.src('./src/*.js')
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'));
+});
 gulp.task('default', function() {
   return gulp.src(['test/test-*.js'], { read: false })
     .pipe(mocha({
