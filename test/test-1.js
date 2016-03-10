da6c2daebf3c5ca736e8a6e9518c7f389459bd63
michelepagot <michele.pagot@hotmail.it>
2016-03-10 01:04:30 +0100

    Add bower files, npm and first test

---
+++ test/test-1.js
@@ -0,0 +1,26 @@
+var assert = require('assert');
+describe('Array', function() {
+  describe('#indexOf()', function () {
+    it('should return -1 when the value is not present', function () {
+      assert.equal(-1, [1,2,3].indexOf(5));
+      assert.equal(-1, [1,2,3].indexOf(0));
+    });
+  });
+  
+  describe('#indexOf()s', function() {
+    it('should return -1 when the value is not present', function() {
+      [1,2,3].indexOf(5).should.equal(-1);
+      [1,2,3].indexOf(0).should.equal(-1);
+    });
+  });
+  
+});
+
+describe('User', function() {
+  describe('#save()', function() {
+    it('should save without error', function(done) {
+      var user = new User('Luna');
+      user.save(done);
+    });
+  });
+});
